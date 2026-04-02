b120baa15aa29820afd7e0808c6b3cba1aa0d521
grcubes <gustavosamrod@gmail.om>
2026-04-02 13:06:07 -0700

    Apply viewport and resize with window

--- core/src/main/java/comp2522/shapes/ShapeViewer.java
+++ core/src/main/java/comp2522/shapes/ShapeViewer.java
@@ -1,13 +1,16 @@
 package comp2522.shapes;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class ShapeViewer extends ApplicationAdapter {
     private SpriteBatch batch;
+    private ExtendViewport viewport;
     private Texture image;
 
     @Override
@@ -19,9 +22,26 @@ public class ShapeViewer extends ApplicationAdapter {
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        viewport.apply();
+
+        logic();
+        draw();
+    }
+
+    private void logic() {
+        float delta = Gdx.graphics.getDeltaTime();
+
+    }
+
+    private void draw() {
         batch.begin();
         batch.draw(image, 140, 210);
         batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
     }
 
     @Override
